641b3544ddc5fb07eeff7607fe5eb184af7baf88
umavictor <umavictor11@gmail.com>
2020-08-14 21:43:59 +0100

    styled url

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Cyan: "hsl(180, 66%, 49%)",
+        DarkViolet: "hsl(257, 27%, 26%)",
+        Red: "hsl(0, 87%, 67%)",
+        Gray: "hsl(0, 0%, 75%)",
+        Grayish: "hsl(225deg 33% 95%)",
+        GrayishViolet: "hsl(257, 7%, 63%)",
+        VeryDarkBlue: "hsl(255, 11%, 22%)",
+        VeryDarkViolet: "hsl(260, 8%, 14%)",
+      },
+      spacing: {
+        7: "2rem",
+        72: "20rem",
+        90: "90%",
+        85: "85%",
+      }
+    },
   },
   variants: {},
   plugins: [],
-}
+};
